ec53e428082a9b81e7a838da5f68cc6a033d5a01
chongswee <chongsweeswee@gmail.com>
2018-01-16 12:04:41 +0800

    added merge comment 2

--- lib/hellorb.rb
+++ lib/hellorb.rb
@@ -2,6 +2,6 @@ require 'greeter'
 
 # Default is World
 name = ARGV.first || "World"
-# this is a new comment to be merged 
+#EDITTED  this EDITTED  is a new comment to be merged 
 greeter = Greeter.new(name)
 puts greeter.greet
